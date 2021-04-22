64f30a14d3b56cb8fda8648f0d09a20d6e47d266
jake howlett <jake.howlett93@gmail.com>
2021-04-22 09:23:55 +0100

    create search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import Search from './components/search';
 
 function App() {
   return (
     <div>
-
+      <div className="left-scene">
+        <Search />
+      </div>
+      <div className="right-scene"></div>
     </div>
   );
 }
