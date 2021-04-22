6ca47e08b6c21fca74367133c81cb46ac222685c
jake howlett <jake.howlett93@gmail.com>
2021-04-22 10:27:27 +0100

    create recipe list component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Search from './components/search';
 import Recipe from './components/recipe';
+import RecipeList from './components/recipe_list';
 
 function App() {
   return (
     <div>
       <div className="left-scene">
         <Search />
+        <RecipeList />
       </div>
       <div className="right-scene">
         <div className="selected-recipe">
---
+++ src/components/recipe_list.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RecipeList = () => {
+  return (
+    <p>List of recipes goes here</p>
+  );
+}
+
+export default RecipeList;
